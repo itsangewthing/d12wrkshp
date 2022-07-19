7fc7a3defd8c971ab312305333e1fc405b0a8975
partytaitai <partytaitai@MacBook-Pro-3.local>
2022-07-19 16:26:51 +0800

    workshop up

--- src/main/java/sg/edu/nus/iss/day12wkshp/WorkshopController.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/WorkshopController.java
@@ -1,7 +1,12 @@
 package sg.edu.nus.iss.day12wkshp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -11,4 +16,34 @@ public class WorkshopController {
     public String test() {
         return "workshop";
     }
+
+    @RequestMapping(path={"/processNumber"})
+    public String testResults(
+        @RequestParam(name="inputNumber", defaultValue="1") Integer unit, Model model){
+
+            List<Integer> lstInt = new ArrayList<>();
+            int loopValue = Integer.parseInt(unit.toString());
+
+            int loop = 1;
+            while (loop <= loopValue) {
+                int result = (int) (Math.random() * (loopValue) + 1);    
+                
+                //to check if the # is duplicated, it wont be added.
+                if(!lstInt.contains(Integer.valueOf(result))){
+                    lstInt.add(Integer.valueOf(result));
+                    loop++;
+                }
+            }
+
+            for (int i = 0; i < lstInt.size(); i++) {
+                System.out.print("> " + lstInt.get(i));
+                System.out.println();
+            }
+
+            model.addAttribute("numbers", lstInt);
+
+            return "workshopresult";
+        }
+        
+    
 }
